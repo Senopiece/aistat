c570b93a3e450d2db25e41a43161762cdcfb88a0
Senopiece <shichiromail@gmail.com>
2022-10-31 23:52:48 +0300

    1

--- VitalyMahonin.py
+++ VitalyMahonin.py
@@ -1,52 +1,57 @@
 #!/bin/python3
+from dataclasses import dataclass
 import mido
 import argparse
-
-
-## Constants
-
-
-MINOR_CHORD = (0, 3, 7)
-MAJOR_CHORD = (0, 4, 7)
-DIM_CHORD = (0, 3, 6)
-
-CHORDS = (MINOR_CHORD, MAJOR_CHORD, DIM_CHORD)
 
 
 ## Helpers
 
 
+@dataclass
 class Key:
-    name = None
-    notes = None
+    name: str
+    tonic: int
+    notes: set[int]
 
-    def __init__(self, name, notes):
+    def __init__(self, name: str, tonic: int, scale: tuple[int]):
+        """Create a key from a tonic and a scale.
+
+        Args:
+            name (str): the string representation of the key
+            tonic (int): the MIDI note number of the tonic
+            scale (tuple[int]): list of offsets from the tonic
+
+            example of a possible scale:
+                major scale: (0, 2, 4, 5, 7, 9, 11)
+        """
+        assert len(scale) == 7
         self.name = name
-        self.notes = notes
+        self.tonic = tonic
+        self.notes = set([(e + tonic) % 12 for e in scale])
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
-def list_diff(list1, list2):
+def list_diff(list1: list, list2: list) -> int:
     diff = 0
     for elem1, elem2 in zip(list1, list2):
         diff += abs(elem1 - elem2)
     return diff
 
 
-def detect_key(notes):
+def detect_key(notes: list[int]) -> Key:
     """Detects one of 24 keys,
     but loses the octave information
 
     Args:
-        notes (List[int]): List of used notes (midi numbers)
+        notes (list[int]): List of used notes (midi numbers)
 
     Returns:
         Key: the most appropriate key
     """
     _notes_names = ("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B")
-    _offsets = (
+    _scales = (
         (0, 2, 4, 5, 7, 9, 11),  # major
         (0, 2, 3, 5, 7, 8, 10),  # minor
     )
@@ -62,28 +67,29 @@ def detect_key(notes):
 
     # determine scale
     original_offset = [e - most_freq[0] for e in most_freq]
-    scale = 0
+    scale_index = 0
     score = 10000  # minimizing the score
-    for i, offset in enumerate(_offsets):
+    for i, offset in enumerate(_scales):
         new_score = list_diff(original_offset, offset)
         if new_score < score:
-            scale = i
+            scale_index = i
             score = new_score
 
     # determine tonic
     tonic = most_freq[0]
 
     return Key(
-        f"{_notes_names[tonic]}{_offsets_names[scale]}",
-        [(e + tonic) % 12 for e in _offsets[scale]],
+        f"{_notes_names[tonic]}{_offsets_names[scale_index]}",
+        tonic,
+        _scales[scale_index],
     )
 
 
-def detect_octave(notes):
+def detect_octave(notes: list[int]) -> int:
     """Detects the average octave of the notes
 
     Args:
-        notes (List[int]): List of used notes (midi numbers)
+        notes (list[int]): List of used notes (midi numbers)
 
     Returns:
         int: the octave of the notes
@@ -92,6 +98,28 @@ def detect_octave(notes):
     for note in notes:
         s += note // 12
     return s // len(notes)
+
+
+MINOR_CHORD = (0, 3, 7)
+MAJOR_CHORD = (0, 4, 7)
+DIM_CHORD = (0, 3, 6)
+
+
+@dataclass
+class Chord:
+    root_note: int
+    notes: set[int]
+
+    def __init__(self, root_note: int, chord: tuple[int]):
+        """Create a chord object
+
+        Args:
+            root_note (int): midi index of the root note
+            chord (list[int]): triplets of offsets from the root note
+        """
+        assert len(chord) == 3
+        self.root_note = root_note
+        self.notes = set([(root_note + offset) % 12 for offset in chord])
 
 
 ## Main
