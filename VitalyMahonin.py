2098cfb505073905e19d10f5e61de27cd52cdcce
Senopiece <shichiromail@gmail.com>
2022-11-01 23:11:46 +0300

    works fine

--- VitalyMahonin.py
+++ VitalyMahonin.py
@@ -1,7 +1,11 @@
 #!/bin/python3
+from copy import deepcopy
 from dataclasses import dataclass
-import mido
+import math
+import random
 import argparse
+import mido
+from typing import Callable, Iterator, Union
 
 
 ## Helpers
@@ -11,7 +15,7 @@ import argparse
 class Key:
     name: str
     tonic: int
-    notes: set[int]
+    notes: frozenset[int]
 
     def __init__(self, name: str, tonic: int, scale: tuple[int]):
         """Create a key from a tonic and a scale.
@@ -24,10 +28,9 @@ class Key:
             example of a possible scale:
                 major scale: (0, 2, 4, 5, 7, 9, 11)
         """
-        assert len(scale) == 7
         self.name = name
         self.tonic = tonic
-        self.notes = set([(e + tonic) % 12 for e in scale])
+        self.notes = frozenset((e + tonic) % 12 for e in scale)
 
     def __str__(self) -> str:
         return self.name
@@ -40,12 +43,12 @@ def list_diff(list1: list, list2: list) -> int:
     return diff
 
 
-def detect_key(notes: list[int]) -> Key:
+def detect_key(notes: Iterator[int]) -> Key:
     """Detects one of 24 keys,
     but loses the octave information
 
     Args:
-        notes (list[int]): List of used notes (midi numbers)
+        notes (Iterator[int]): List of used notes (midi numbers)
 
     Returns:
         Key: the most appropriate key
@@ -65,7 +68,7 @@ def detect_key(notes: list[int]) -> Key:
     most_freq = sorted(range(12), key=lambda x: freq[x], reverse=True)[:7]
     most_freq.sort()
 
-    # determine scale
+    # determine scale by choosing the most appropriate match
     original_offset = [e - most_freq[0] for e in most_freq]
     scale_index = 0
     score = 10000  # minimizing the score
@@ -85,51 +88,255 @@ def detect_key(notes: list[int]) -> Key:
     )
 
 
-def detect_octave(notes: list[int]) -> int:
+def detect_octave(notes: Iterator[int]) -> int:
     """Detects the average octave of the notes
 
     Args:
-        notes (list[int]): List of used notes (midi numbers)
+        notes (Iterator[int]): List of used notes (midi numbers)
 
     Returns:
         int: the octave of the notes
     """
     s = 0
+    n = 0
     for note in notes:
         s += note // 12
-    return s // len(notes)
+        n += 1
+    return s // n
 
 
 MINOR_CHORD = (0, 3, 7)
 MAJOR_CHORD = (0, 4, 7)
 DIM_CHORD = (0, 3, 6)
+SUS2_CHORD = (0, 2, 7)
+SUS4_CHORD = (0, 5, 7)
 
 
 @dataclass
 class Chord:
+    octave: int
     root_note: int
-    notes: set[int]
+    notes: frozenset[int]
 
-    def __init__(self, root_note: int, chord: tuple[int]):
+    def __init__(self, root_note: int, chord: tuple[int], octave: int):
         """Create a chord object
 
         Args:
-            root_note (int): midi index of the root note
-            chord (list[int]): triplets of offsets from the root note
+            root_note (int): midi index of the root note (octave lost)
+            chord (tuple[int]): triplets of offsets from the root note
+            octave(int): octave of the chord
         """
         assert len(chord) == 3
+        assert 0 <= root_note < 12
         self.root_note = root_note
-        self.notes = set([(root_note + offset) % 12 for offset in chord])
+        self.octave = octave
+        self.notes = frozenset((root_note + offset) % 12 for offset in chord)
+
+    def __hash__(self) -> int:
+        return hash((self.root_note, self.notes))
+
+
+## Genetic stuff
+# instances are tuple[Chord]
+
+
+def random_chord() -> Union[Chord, None]:
+    return (
+        None
+        if random.randint(0, 1) == 0
+        else Chord(
+            random.randint(0, 11),
+            random.choice(
+                (
+                    MINOR_CHORD,
+                    MAJOR_CHORD,
+                    DIM_CHORD,
+                    SUS2_CHORD,
+                    SUS4_CHORD,
+                )
+            ),
+            random.randint(0, 8),
+        )
+    )
+
+
+class AccompanimentFitnessChecker:
+    melody_key: Key
+    key_notes: tuple[int]  # may contain None's TODO
+    melody_octave: int
+
+    def __init__(self, melody_key: Key, key_notes: tuple[int], melody_octave: int):
+        """Creates the accompaniment fitness checker for a concrete melody
+
+        Args:
+            melody_key (Key): the key of the melody
+            key_notes (tuple[int]): notes (midi, lost octave) that need to be consonant with the accompaniment
+            melody_octave (int): midi octave of the melody
+        """
+        assert all(note is None or 0 <= note < 12 for note in key_notes)
+        self.melody_key = melody_key
+        self.key_notes = key_notes
+        self.melody_octave = melody_octave
+
+    def fitness(self, accompaniment: tuple[Chord]) -> float:
+        assert len(accompaniment) == len(self.key_notes)
+        octave = None
+        score = 0
+        for i, chord in enumerate(accompaniment):
+            if chord is not None:
+                if chord.notes < self.melody_key.notes:
+                    score += 1
+                if octave is None:
+                    octave = chord.octave
+                if chord.octave == self.melody_octave - 1:
+                    score += 1
+                if chord.octave == self.melody_octave - 2:
+                    score += 0.7
+                if chord.octave == self.melody_octave - 3:
+                    score += 0.5
+                if octave == chord.octave:
+                    score += 2
+                if chord.root_note == self.key_notes[i]:
+                    score += 10
+            elif self.key_notes[i] is None:
+                score += 10
+        return score
+
+
+def generate_random_population(
+    population_size: int,
+    instance_len: int,
+) -> set[tuple[Chord]]:
+    """Returns a random population of accompaniments
+
+    Args:
+        population_size (int): size of the resulting set
+        instance_len (int): length of each instance of the population
+
+    Returns:
+        set[tuple[Chord]]: the resulting set
+    """
+    res = set()
+    for _ in range(population_size):
+        res.add(
+            tuple(random_chord() for _ in range(instance_len)),
+        )
+    return res
+
+
+def select(
+    population: set[tuple[Chord]],
+    fitness: Callable[[tuple[Chord]], int],
+) -> set[tuple[Chord]]:
+    """Selects 50% of the best instances from the population according to the fitness function
+
+    Args:
+        population (set[tuple[Chord]]): parent population
+        fitness (Callable[[tuple[Chord]], int]): fitness function ( higher is better )
+
+    Returns:
+        set[tuple[Chord]]: the set of selected instances
+    """
+    return set(sorted(population, key=fitness, reverse=True)[: len(population) // 2])
+
+
+def cross(parents: tuple[tuple[Chord]]) -> tuple[Chord]:
+    """Create a new instance crossing parents
+
+    Args:
+        parents (set[tuple[Chord]]): the parents
+
+    Returns:
+        tuple[Chord]: crossed instance
+    """
+    assert len(parents) > 1
+    instance_len = len(parents[0])
+    assert all(instance_len == len(parent) for parent in parents)
+    return tuple(random.choice(parents)[i] for i in range(instance_len))
+
+
+def mutate(
+    instance: tuple[Chord],
+) -> tuple[Chord]:
+    """Returns similar instance, but 10% of it's chords are modified
+
+    Args:
+        instance (tuple[Chord]): source instance
+
+    Returns:
+        tuple[Chord]: mutated instance
+    """
+    return tuple(
+        random_chord() if random.randint(0, 10) == 0 else chord for chord in instance
+    )
 
 
 ## Main
 
 
+def track_longing(track: list[mido.Message]) -> int:
+    """Track longing
+
+    Args:
+        track (list[mido.Message]): the track notes
+
+    Returns:
+        int: track longing in midi ticks
+    """
+    ticks = 0
+    for msg in track:
+        ticks += msg.time
+    return ticks
+
+
+def compute_key_notes(track: list[mido.Message], chord_duration: int) -> tuple[int]:
+    """Returns notes that need to be consonant with the accompaniment
+
+    Args:
+        track (list[mido.Message]): the track notes
+        chord_duration (int): duration of a chord in ticks
+
+    Returns:
+        tuple[int]: a tuple of midi notes (lost octave)
+    """
+    notes_list = [None] * (math.ceil(track_longing(track) / chord_duration))
+    ticks = 0
+    for msg in track:
+        ticks += msg.time
+        if ticks % chord_duration == 0 and msg.type == "note_on":
+            notes_list[ticks // chord_duration] = msg.note % 12
+    return tuple(notes_list)
+
+
+def filter_notes(file: mido.MidiFile) -> Iterator[mido.Message]:
+    tempo = None
+    for msg in file:
+        if isinstance(msg, mido.MetaMessage) and msg.type == "set_tempo":
+            tempo = msg.tempo
+            continue
+
+        if isinstance(msg, mido.Message):
+            msg.time = int(
+                mido.second2tick(msg.time, file.ticks_per_beat, tempo)
+            )  # for some reason, the time is in seconds while iterating through raw file, so we need to convert it to ticks
+            yield msg
+
+
+def get_average_velocity(file: mido.MidiFile):
+    sum_velocity = 0
+    n = 0
+    for msg in file:
+        if isinstance(msg, mido.Message) and (msg.type == "note_on"):
+            sum_velocity += msg.velocity
+            n += 1
+    return sum_velocity // n
+
+
 def midi_file(string):
     try:
         return mido.MidiFile(string)
-    except:
-        raise argparse.ArgumentTypeError("Invalid MIDI file")
+    except Exception as exc:
+        raise argparse.ArgumentTypeError("Invalid MIDI file") from exc
 
 
 if __name__ == "__main__":
@@ -159,13 +366,70 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-    args.input.type = 1
+    assert args.input.type != 2
 
-    melody_notes = [
+    melody_notes = tuple(
         msg.note
         for msg in args.input
         if isinstance(msg, mido.Message) and msg.type == "note_on"
-    ]
+    )
 
-    print(detect_key(melody_notes))
-    print(detect_octave(melody_notes))
+    # define chord duration and divide track into equal parts of chord durations
+    chord_duration = args.input.ticks_per_beat * 2
+    key_notes = compute_key_notes(
+        list(filter_notes(args.input)),
+        chord_duration,
+    )
+
+    # run genetic to find a good accompaniment
+    key = detect_key(melody_notes)
+    fitness_checker = AccompanimentFitnessChecker(
+        key,
+        key_notes,
+        detect_octave(melody_notes),
+    )
+
+    population = generate_random_population(100, len(key_notes))
+    for _ in range(1000):
+        selected = select(population, fitness_checker.fitness)
+        population = deepcopy(selected)
+        while len(population) < random.randint(90, 100):
+            population.add(
+                mutate(
+                    cross(
+                        tuple(random.choice(tuple(selected)) for _ in range(2)),
+                    ),
+                ),
+            )
+    best = sorted(population, key=fitness_checker.fitness, reverse=True)[0]
+
+    # write result (as a new track)
+    velocity = int(get_average_velocity(args.input) * 0.9)
+    track = mido.MidiTrack()
+    track.append(mido.MetaMessage("track_name", name="Accompaniment", time=0))
+    start = 0
+    for chord in best:
+        if chord is None:
+            start = chord_duration
+        else:
+            for i, note in enumerate(chord.notes):
+                track.append(
+                    mido.Message(
+                        "note_on",
+                        note=note + chord.octave * 12,
+                        velocity=velocity,
+                        time=start if i == 0 else 0,
+                    )
+                )
+            for i, note in enumerate(chord.notes):
+                track.append(
+                    mido.Message(
+                        "note_off",
+                        note=note + chord.octave * 12,
+                        velocity=velocity,
+                        time=chord_duration if i == 0 else 0,
+                    )
+                )
+            start = 0
+    args.input.tracks.append(track)
+    args.input.save(f"{args.out}.mid")
